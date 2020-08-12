034238b8eb79670647808924500ea265b3f636eb
William Jeffcock <jeffcockw@gmail.com>
2020-08-12 17:25:43 +0100

    Add support to load teams from teams/ and/or teams.yml in plugin folder

--- src/main/java/rip/bolt/ingame/team/ConfigTeamParser.java
+++ src/main/java/rip/bolt/ingame/team/ConfigTeamParser.java
@@ -3,6 +3,7 @@ package rip.bolt.ingame.team;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -18,20 +19,36 @@ public class ConfigTeamParser {
     private static ConfigTeamParser instance;
 
     private ConfigTeamParser() {
-        teams = parseTournamentTeams(new File(Tournament.get().getDataFolder(), "teams"));
+        teams = parseTournamentTeams(new File(Tournament.get().getDataFolder(), "teams"), new File(Tournament.get().getDataFolder(), "teams.yml"));
     }
 
-    private static List<TournamentTeam> parseTournamentTeams(File teamsFolder) {
+    private static List<TournamentTeam> parseTournamentTeams(File teamsFolder, File teamsFile) {
         if (!teamsFolder.exists())
             teamsFolder.mkdirs();
 
         List<TournamentTeam> teamList = new ArrayList<TournamentTeam>();
         for (File child : teamsFolder.listFiles((file) -> file.getName().toLowerCase().endsWith(".yml"))) {
             FileConfiguration config = YamlConfiguration.loadConfiguration(child);
-                String teamName = config.getString("name");
-                List<TournamentPlayer> players = config.getStringList("players").stream().map(String::trim).map(UUID::fromString).map(x -> TournamentPlayer.create(x, true)).collect(Collectors.toList());
+            String teamName = config.getString("name");
+            List<TournamentPlayer> players = config.getStringList("players").stream().map(String::trim).map(UUID::fromString).map(x -> TournamentPlayer.create(x, true)).collect(Collectors.toList());
+
+            teamList.add(TournamentTeam.create(teamName, players));
+        }
+
+        if (teamsFile.exists()) {
+            YamlConfiguration teamsConfig = YamlConfiguration.loadConfiguration(teamsFile);
+            for (Object object : teamsConfig.getList("teams")) {
+                if (!(object instanceof Map<?, ?>)) {
+                    System.out.println("Invalid type in teams.yml (" + object.getClass().getName() + ": " + object.toString() + ")! Skipping...");
+                    continue;
+                }
+
+                Map<Object, Object> team = (Map<Object, Object>) object;
+                String teamName = (String) team.get("name");
+                List<TournamentPlayer> players = ((List<String>) team.get("players")).stream().map(String::trim).map(UUID::fromString).map(x -> TournamentPlayer.create(x, true)).collect(Collectors.toList());
 
                 teamList.add(TournamentTeam.create(teamName, players));
+            }
         }
 
         return teamList;
